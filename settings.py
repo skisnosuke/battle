4ced411a7d415144c11d1d33c9594a1e210149a8
skisnosuke <yakyuman.1108@icloud.com>
2021-10-05 15:12:24 +0900

    ウィンドウの表示

--- settings.py
+++ settings.py
@@ -1,3 +1,11 @@
+import pygame
+class Settings:
+    #設定を格納するクラス
+    def __init__(self):
+        """画面設定"""
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
 # 背景画像
 
 # コマンドライン
