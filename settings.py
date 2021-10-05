570711495b30af838537bc826971d63780f31b40
eteRnaL198 <ikuya313198@gmail.com>
2021-10-05 19:52:21 +0900

    背景暗めにしました

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings:
         """画面設定"""
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (10, 10, 10)
 # 背景画像
 
 # コマンドライン
