9c6de28939f9272924cd6cb42431a63f1bbdeb30
skisnosuke <yakyuman.1108@icloud.com>
2021-10-08 12:57:17 +0900

    画面処理を1つのメソッドに

--- main.py
+++ main.py
@@ -27,25 +27,28 @@ class Battle:
 
         #メインループ
         while True:
-          #キーボード、マウスの監視
-          for event in pygame.event.get():
-              if event.type == pygame.QUIT:
-                  sys.exit()
+            #キーボード、マウスの監視
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
-          #画面のリセット
-          self.screen.fill(self.settings.bg_color)
+            self._update_screen()
 
-          #最新の画面の表示
-          #背景の表示
-          self.screen.blit(self.bg, (0,0))
-          #矩形の表示
-          self.command.draw(self.screen)
-          self.log.draw(self.screen)
-          self.player.draw(self.screen)
-          #テキストの表示
-          self.log.display(self.screen)
-          #更新
-          pygame.display.flip()
+    def _update_screen(self):
+        #画面のリセット
+        self.screen.fill(self.settings.bg_color)
+
+        #最新の画面の表示
+        #背景の表示
+        self.screen.blit(self.bg, (0,0))
+        #矩形の表示
+        self.command.draw(self.screen)
+        self.log.draw(self.screen)
+        self.player.draw(self.screen)
+        #テキストの表示
+        self.log.display(self.screen)
+        #更新
+        pygame.display.flip()
 
 if __name__ == "__main__":
     #ゲームのインスタンスを生成、その後実行する
