2bd0ea853bd5505366b53912badf1343b38177fb
eteRnaL198 <ikuya313198@gmail.com>
2022-02-19 16:32:58 +0900

    位置 修正

--- player.py
+++ player.py
@@ -11,17 +11,13 @@ class Player(Character):
     self.font = self.settings.font
     self.name = self.settings.font.render(self.settings.player_status_name, False, (255, 255, 255))
     self.status_attack = self.settings.player_status_attack
-    # self.font = pygame.font.Font(None, 35)
-
-  #def attack(self):
-    #self.remain_enemy_hp = self.command.enemy_hp - self.command.attacked
 
   def draw(self, screen):
     pygame.draw.rect(screen, (255, 255, 255),Rect(self.settings.player_position+self.settings.player_length), 10)
     pygame.draw.rect(screen, (0,0,0),Rect(self.settings.player_position+self.settings.player_length))
     str_lv = "レベル{:4d}".format(self.settings.player_status_lv)
-    str_hp = "ＨＰ{:5d}".format(self.settings.player_status_hp)
-    str_mp = "ＭＰ{:5d}".format(self.settings.player_status_mp)
+    str_hp = "ＨＰ{:6d}".format(self.settings.player_status_hp)
+    str_mp = "ＭＰ{:6d}".format(self.settings.player_status_mp)
 
     name = self.settings.font.render(self.settings.player_status_name, True, (255, 255, 255))
     hp = self.settings.font.render(str_hp, True, (255, 255, 255))
