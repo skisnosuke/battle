d5a7ad2c941b792384ec8a09811dd345360c1881
eteRnaL198 <ikuya313198@gmail.com>
2022-02-23 15:33:33 +0900

    修正

--- test/test.py
+++ test/test.py
@@ -1,21 +1,22 @@
-from ..character import Character
+import sys
+sys.path.append("src")
+from unittest import TestCase, main
+from character import Character # vscode上で警告出るが無視
 
-passed = 0
-failed = 0
+class Test(TestCase):
+  def test_example(self): # メソッド名は必ずtest_で始める
+    actual = 1 + 1 # 実行結果を代入
+    expected = 2 # 期待する値
+    self.assertEqual(expected, actual) # 等しければPassed
 
-print("こうげき")
-player = Character(10, "p", 1, 0) # (attack, name, hp, mp)
-enemy = Character(0, "e", 10, 0) # (attack, name, hp, mp)
-player.attack(enemy)
-result = enemy.hp
-if(result == 0):
-  print("Passed")
-  passed += 1
-else:
-  print("Error result: %d" % result)
-  failed += 1
-
-print("呪文")
-# ここから書く
-
-print("Passed: %d, Failed: %d" % (passed, failed))
+  def test_player_attack_reduces_enemy_hp_by_player_status_attack(self):
+    # 主人公の攻撃力の分だけ敵のHPが減る
+    player = Character(10, "p", 1, 0) # (attack, name, hp, mp)
+    enemy = Character(0, "e", 10, 0) # (attack, name, hp, mp)
+    player.attack(enemy)
+    actual = enemy.hp
+    expected = 0
+    self.assertEqual(expected, actual)
+  
+if __name__ == "__main__":
+  main()
