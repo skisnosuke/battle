cbb79bacf2083aee58ad8a75a8123a0803b08e2a
eteRnaL198 <ikuya313198@gmail.com>
2022-02-23 14:10:15 +0900

    テスト記述

--- test/test.py
+++ test/test.py
@@ -1,13 +1,21 @@
+from ..character import Character
+
 passed = 0
 failed = 0
 
-print("1+1=2になるように")
-result = 1 + 1
-if(result == 2):
+print("こうげき")
+player = Character(10, "p", 1, 0) # (attack, name, hp, mp)
+enemy = Character(0, "e", 10, 0) # (attack, name, hp, mp)
+player.attack(enemy)
+result = enemy.hp
+if(result == 0):
   print("Passed")
   passed += 1
 else:
   print("Error result: %d" % result)
   failed += 1
+
+print("呪文")
+# ここから書く
 
 print("Passed: %d, Failed: %d" % (passed, failed))
