e218ccf617e6441150b644ae789872d34aa826ad
skisnosuke <yakyuman.1108@icloud.com>
2022-03-04 21:45:34 +0900

    呪文のテストの追加

--- test/test.py
+++ test/test.py
@@ -17,6 +17,14 @@ class Test(TestCase):
     actual = enemy.hp
     expected = 0
     self.assertEqual(expected, actual)
+
+  def test_player_cast_spell_reduce_enemy_hp_by_mera_damage(self):
+    player = Character(0, "p", 1, 10)
+    enemy = Character(0, "e", 10, 0)
+    player.cast_spell(enemy, "mera")
+    actual = enemy.hp
+    expected = 0
+    self.assertEqual(expected, actual)
   
 if __name__ == "__main__":
   main()
