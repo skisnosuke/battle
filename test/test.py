46d43c44b9c3913f42094ba26a42b6c3d6876b1a
skisnosuke <yakyuman.1108@icloud.com>
2022-04-11 13:41:11 +0900

    mpのテストの追加

--- test/test.py
+++ test/test.py
@@ -19,6 +19,7 @@ class Test(TestCase):
     self.assertEqual(expected, actual)
 
   def test_player_cast_spell_reduces_enemy_hp_by_mera_damage(self):
+    # メラのダメージだけ敵のHPが減る
     player_spells = {
             "mera": { "label": "メラ", "mp": 2, "damage": 10, "type": "attack" },
             }
@@ -28,6 +29,20 @@ class Test(TestCase):
     actual = enemy.hp
     expected = 0
     self.assertEqual(expected, actual)
+
+  def test_player_cast_spell_reduces_players_mp_by_mera_cosume(self):
+    # メラの消費mpだけプレイヤーのmpが減る
+    player_spells = {
+            "mera": { "label": "メラ", "mp": 2, "damage": 10, "type": "attack" },
+            }
+    player = Character(0, "p", 1, 2, player_spells)
+    enemy = Character(0, "e", 10, 0, player_spells)
+    player.cast_spell(enemy, "mera")
+    actual = player.mp
+    expected = 0
+    self.assertEqual(expected, actual)
+
   
+
 if __name__ == "__main__":
   main()
